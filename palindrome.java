9264bb5f69c4b08f50117626fa7cd557cf1c3509
ull0sm <ullas4101997@gmail.com>
2024-11-15 02:52:41 +0000

    errored code

--- palindrome.java
+++ palindrome.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 class palindrome{
+    public static int string_length(String s){
+        int n = 0 ;
+        do {
+            n = n+1;
+        } while (s.charAt(n)!= '\0');
+        return n;
+    }
+    public static boolean ispalindrome(String s){
+        return true;
+        
+    }    
     public static void main(String[] args) {
-        System.err.println("hello world!!!!");
+        System.out.println("enter the word/number : ");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        if (ispalindrome(word)) {
+            System.out.println("your word is a palindrome");
+        }
+        else{
+            System.out.println("nope");
+        }
+        System.out.println(string_length(word));
     }
 }
