bd14239d5c0e6ebbbed4a1db673161a0dde5e932
ull0sm <ullas4101997@gmail.com>
2024-12-16 01:33:53 +0000

    reversing an array

---
+++ revarr.java
@@ -0,0 +1,8 @@
+class revarr {
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3 };
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.println(a[i]);
+        }
+    }
+}
